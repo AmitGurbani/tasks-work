9a238f56659cc6dce9d24841210e114735799344
Amit Gurbani <amit1994.gurbani@gmail.com>
2023-03-30 15:05:30 +0530

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -18,6 +18,6 @@ import App from "../components/app.svelte";
 Framework7.use(Framework7Svelte);
 
 // Mount Svelte App
-new App({
+const _app = new App({
   target: document.getElementById("app"),
 });
